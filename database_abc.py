d6c6e0eb5c0e8499de39d844719f2f0067089c74
Andrew Dimery <Andrewbrewaskew@gmail.com>
2020-02-18 08:59:01 -0600

    Create ABC ArtistDB

---
+++ database_abc.py
@@ -0,0 +1,24 @@
+""" 
+Creating an abstract class to interface with the SQL databse. 
+This should all for an easy swap to peewee. 
+"""
+
+import abc
+# Artist is an object with Artist and Email as artributes
+# Artwork is an object with artist, artwork_name, 
+class ArtistDB(abc.ABC):
+    @abc.abstractmethod
+    def add_Artist(self, Artist):
+        pass
+    @abc.abstractmethod
+    def search_Artwork(self, artist_search):
+        pass
+    @abc.abstractmethod
+    def displayAvailable(self, artist_search):
+        pass
+    @abc.abstractmethod
+    def add_new_artwork(self , artist):
+        pass
+    @abc.abstractmethod
+    def change_availblity(self):
+        pass
