07697f1ffd6183bcc9eaf06a4631e0c78db83387
Andrew Dimery <Andrewbrewaskew@gmail.com>
2020-02-18 08:53:23 -0600

    Create Artwork Class

---
+++ model/artist_model.py
@@ -0,0 +1,20 @@
+"""
+Forming an object to interact with the database. This represents the artist
+table.
+"""
+
+class Artists():
+    def __init__(self):
+        self.name = name
+        self.email = email
+
+    def __str__ (self):
+        return f'{self.name}, {self.email}'
+        
+
+class Artwork ():
+    def __init__(self):
+        self.artist_name = artist_name
+        self.artwork_name = artwork_name
+        self.price = price 
+        self.availablity = availablity
